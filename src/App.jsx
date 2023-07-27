00fdab32543f7751ba3315b6bb30f34ae554897c
AmanKamble <amankamble88@gmail.com>
2023-07-27 09:51:36 +0530

    complete
    Brand page is completed

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,17 @@ const App = () => {
           <div className='text'>
             <h1>YOUR FEET DESERVE THE BEST</h1>
             <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <>
+              <button>Shop Now</button>
+              <button className='category'>Category</button>
+            </>
+            <div className="bottom">
+              <p>Also Available On</p>
+              <>
+                <img src="/images/flipkart.png" alt="" />
+                <img src="/images/amazon.png" alt="" />
+              </>
+            </div>
           </div>
           <div className='image'>
             <img src="/images/shoe_image.png" alt="" srcset="" />
