f35cbec7f4b2f6b3c502de8b1d567a569c60bdb2
AmanKamble <amankamble88@gmail.com>
2023-07-27 09:28:34 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      {/* Navbar */}
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+
+        <ul>
+          <li><a href="">MENU</a></li>
+          <li><a href="">LOCATION</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+
+      {/* Main */}
+      <main>
+        <div className="container">
+          <div className='text'>
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+          </div>
+          <div className='image'>
+            <img src="/images/shoe_image.png" alt="" srcset="" />
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
